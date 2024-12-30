c980edb7d338e1f11b4d8dc3df5e872c274e9c9e
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-30 20:44:24 +0600

    Init the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  return (
+   <div>
+    <h1>Counter with Redux</h1>
+    <button>Increment</button>
+    <div>
+      0
+    </div>
+    <button>Decrement</button>
+   </div>
+  )
+}
+
+export default App
