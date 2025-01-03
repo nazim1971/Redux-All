f0aa482290b0558654c944ab76cf2441e731a66f
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-04 02:27:34 +0600

    Add Nav details

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="text-2xl">
+                Task <span className="text-red-500">Master</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
