3b2ae13ee0d0e5ee71f1d68a5a07aeb5bb529448
nazim1971 <md.nazimuddinaj@gmail.com>
2025-01-04 02:25:05 +0600

    Add Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { decrement, increment } from "./redux/features/counter/counterSlice";
 import { RoootState } from "./redux/store";
 import { useAppDispatch, useAppSelector } from "./redux/hook";
+import { Button } from "./components/ui/button";
 
 function App() {
 
@@ -20,12 +21,12 @@ function App() {
   return ( 
    <div className="px-10 mt-10">
     <h1>Counter with Redux</h1>
-    <button className="btn bg-slate-200 p-2 m-2 rounded-2xl" onClick={()=>handleIncrement (2) } >Increment by 2</button>
-    <button className="btn bg-slate-200 p-2 m-2 rounded-2xl" onClick={()=>handleIncrement (5) } >Increment by 5</button>
+    <Button className="btn bg-slate-200 p-2 m-2 rounded-2xl" onClick={()=>handleIncrement (2) } >Increment by 2</Button>
+    <Button className="btn bg-slate-200 p-2 m-2 rounded-2xl" onClick={()=>handleIncrement (5) } >Increment by 5</Button>
     <div>
       {count}
     </div>
-    <button className="btn bg-slate-200 p-2 m-2 rounded-2xl" onClick={()=> handledecrement(5)} >Decrement</button>
+    <Button className="btn bg-slate-200 p-2 m-2 rounded-2xl" onClick={()=> handledecrement(5)} >Decrement</Button>
    </div>
   )
 }
